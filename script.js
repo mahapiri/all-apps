4a45d2c7b451755f6cdc633b7ce0e4a5743de164
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-01-13 23:51:11 +0100

    added css

---
+++ script.js
@@ -0,0 +1,16 @@
+function init() {
+    openMenu();
+}
+
+
+function openMenu() {
+    let menu = document.getElementById('menu');
+    let menubar = document.getElementById('menu-bar');
+    let close = document.getElementById('close-button');
+    menu.addEventListener("click", () => {
+        menubar.classList.toggle('d-none');
+    });
+    close.addEventListener("click", () => {
+        menubar.classList.toggle('d-none');
+    })
+}
