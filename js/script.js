d9521bee3b6009c74f93c586203a416f65aa6442
Pirathib Mahalingam <pirathib.m@gmail.com>
2024-01-19 16:39:10 +0100

    added all pages,js, css

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 function init() {
     includeHTML();
-    openMenu();
 }
 
 
 function openMenu() {
-    let menu = document.getElementById('menu');
-    let menubar = document.getElementById('menu-bar');
-    let close = document.getElementById('close-button');
-    menu.addEventListener("click", () => {
-        menubar.classList.toggle('d-none');
-    });
-    close.addEventListener("click", () => {
-        menubar.classList.toggle('d-none');
-    })
+    let menu = document.getElementById('menu-bar');
+    menu.classList.toggle('d-none');
+    close.classList.toggle('')
+}
+
+function closeMenu() {
+    let menu = document.getElementById('menu-bar');
+    menu.classList.toggle('d-none');
+    close.classList.toggle('')
 }
